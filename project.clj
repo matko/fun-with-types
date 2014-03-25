dd45aab48e3c39f47aadafc38d23b51b9f8b243c
Matthijs van Otterdijk <matko@pievolution.org>
2014-03-25 16:50:11 +0100

    clojure version bump

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A toy project to see if the Extended Calculus of Constructions can be used to build a lisp-like programming language."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
