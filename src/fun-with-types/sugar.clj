e67c31129e5bdcdc196400bf8a48b144cbab80ae
Matthijs van Otterdijk <matko@pievolution.org>
2014-03-27 22:04:19 +0100

    syntactic sugar for multiple argument functions and tuples

---
+++ src/fun-with-types/sugar.clj
@@ -0,0 +1,52 @@
+(ns fun-with-types.sugar
+  (:refer-clojure :exclude [reduce])
+  (:require [fun-with-types.ecc :refer [expr check reduce substitute sum-expression?]]))
+
+
+(defmacro ecc-macro [name [& args] expansion]
+  `(expr ~name [~@args]
+         :check (check ~expansion ~'c)
+         :reduce (reduce ~expansion ~'c)
+         :subsitute (substitute ~expansion ~'sym ~'replacement)))
+
+(defn functional-sugar-fn [expand-sym vars-and-types result]
+  (loop [result result
+         [[var type] & vars-and-types] 
+         (reverse (partition 2 vars-and-types))]
+    (if var
+      (recur `(~expand-sym [~var ~type]
+                        ~result)
+             vars-and-types)
+      result)))
+
+(defmacro functional-sugar [name expand-sym]
+  `(ecc-macro ~name [[& vars-and-types#] result#]
+              (common-sugar-fn '~expand-sym vars-and-types# result#)))
+
+;;these are syntactically very similar, no sense in duplicating code.
+(functional-sugar function lambda)
+(functional-sugar ftype product)
+
+(ecc-macro struct [& vars-and-types]
+           (loop [result (last vars-and-types)
+                  [[var type] & vars-and-types]
+                  (reverse (partition 2 vars-and-types))]
+             (if var
+               (recur `(~'sum [~var ~type]
+                              ~result)
+                      vars-and-types)
+               result)))
+
+(ecc-macro tuple [struct-type & elements]
+           (let [sum-type (reduce struct-type)]
+             (assert (odd? (count elements)))
+             (letfn [(build-pairs [[_ [_ _] right-type :as sum-type] [element & elements]]
+                       (if (seq elements)
+                         `(~'pair ~sum-type
+                                  ~element
+                                  ~(if (> (count elements) 1)
+                                     (build-pairs (if (sum-expression? right-type)
+                                                    right-type)
+                                                  elements)
+                                    (first elements)))))]
+               (build-pairs sum-type elements))))
