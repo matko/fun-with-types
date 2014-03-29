9feb2d320de879cbee8af4358e9698a66f3d924d
Matthijs van Otterdijk <matko@pievolution.org>
2014-03-29 18:30:20 +0100

    Added midje dependencies for testing.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "A toy project to see if the Extended Calculus of Constructions can be used to build a lisp-like programming language."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.1"]]}})
