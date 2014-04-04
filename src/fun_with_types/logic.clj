0815127cafa2dfa75092fd3d4ff97003b519eccc
Matthijs van Otterdijk <matko@pievolution.org>
2014-04-04 06:35:55 +0200

    moved more of logic.clj into logic. only things remaining are macros - to be eliminated later

--- src/fun_with_types/logic.clj
+++ src/fun_with_types/logic.clj
@@ -7,40 +7,21 @@
            `(~'product [~var ~type]
                        ~prop))
 
-(ecc-constant implies2
-              (function [p1 Prop p2 Prop]
-                        (forall [x p1] p2)))
-
 (defn expand-nary [sym fst scnd lst]
   `(~sym ~fst ~(if (seq lst)
                  (expand-nary sym scnd (first lst) (next lst))
                  scnd)))
-
-(defmacro ecc-nary [name expansion]
-  `(ecc-macro ~name [fst# scnd# & lst#]
-              (expand-nary '~expansion fst# scnd# lst#)))
-
-(ecc-nary implies implies2)
-
-
-(ecc-constant and2
-              (function [p1 Prop p2 Prop]
-                        (forall [X Prop]
-                                (implies (implies p1 p2 X)
-                                         X))))
-
-(ecc-nary and and2)
-
-(ecc-constant or2
-              (function [p1 Prop p2 Prop]
-                        (forall [X Prop]
-                                (implies (implies p1 X)
-                                         (implies p2 X)
-                                         X))))
-(ecc-nary or or2)
 
 (ecc-macro exists [[x type] prop]
            `(~'forall [X# ~'Prop]
                       (~'implies (~'forall [~x ~type]
                                            (~'implies ~prop X#))
                                  X#)))
+
+(defmacro ecc-nary [name expansion]
+  `(ecc-macro ~name [fst# scnd# & lst#]
+              (expand-nary '~expansion fst# scnd# lst#)))
+
+(ecc-nary implies implies2)
+(ecc-nary and and2)
+(ecc-nary or or2)
